d1842d5e2e77fbad42a572acc0cabc21ac3a2c31
anoctopi <s-ialam@lwsd.org>
2023-11-29 01:44:07 -0500

    general groundwork for seller has been laid out - sql still missing

--- Client.java
+++ Client.java
@@ -35,51 +35,6 @@ public class Client extends JComponent implements Runnable {
                 "s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9"
         };
 
-        JPanel selSide = createCardPanel("Calendar: Seller");
-        JButton confirm = new JButton("Confirm");
-        //----------------------------------------------------------//
-        selSide.add(confirm);
-        //add this button to every single card - it's going to be
-        //what sends the request
-
-        //----------------------------------------------------------//
-        JComboBox<String> sellerOptions = new JComboBox<>(selOp);
-        JLabel resultLabel = new JLabel("");
-        confirm.addActionListener(e -> {
-            String selectedOption = (String) sellerOptions.getSelectedItem();
-            //SEND REQUEST HERE
-            //
-            //WAIT FOR REPLY THEN
-            //cardLayout.show(THE PANEL SELECTED HERE)
-            resultLabel.setText("Selected Option: " + selectedOption);
-            cardLayout.show(mainPanel, selPans[Arrays.asList(selOp).indexOf(selectedOption)]);
-        });
-
-        selSide.add(sellerOptions);
-        selSide.add(resultLabel);
-        mainPanel.add(selSide);
-        //----------------------------------------------------------//
-        JPanel s0 = createCardPanel(selOp[0]);
-
-        //----------------------------------------------------------//
-        JPanel s1 = createCardPanel(selOp[1]);
-
-        //----------------------------------------------------------//
-        JPanel s2 = createCardPanel(selOp[2]);
-
-        //----------------------------------------------------------//
-        JPanel s3 = createCardPanel(selOp[3]);
-
-        //----------------------------------------------------------//
-        //etc etc
-        //----------------------------------------------------------//
-
-
-        mainPanel.add(s0, "s0");
-        mainPanel.add(s1, "s1");
-        mainPanel.add(s2, "s2");
-        mainPanel.add(s3, "s3");
-        frame.add(mainPanel);
 
         //maybe exit works?
         frame.setDefaultCloseOperation(exit());
@@ -127,6 +82,110 @@ public class Client extends JComponent implements Runnable {
                 frame.add(cb);
             }
         });
+
+        JPanel selSide = createCardPanel("Calendar: Seller");
+        JButton confirm = new JButton("Confirm");
+        //----------------------------------------------------------//
+        selSide.add(confirm);
+        //add this button to every single card - it's going to be
+        //what sends the request
+
+        //----------------------------------------------------------//
+        JComboBox<String> sellerOptions = new JComboBox<>(selOp);
+        JLabel resultLabel = new JLabel("");
+        confirm.addActionListener(e -> {
+            String selectedOption = (String) sellerOptions.getSelectedItem();
+            int choice = Arrays.asList(selOp).indexOf(selectedOption);
+
+            //SEND REQUEST HERE
+            switch (choice) {
+                case 0 -> {
+
+                }
+                case 1 -> {
+
+                }
+                case 2 -> {
+
+                }
+                case 3 -> {
+
+                }
+                case 4 -> {
+
+                }
+                case 5 -> {
+
+                }
+                case 6 -> {
+
+                }
+                case 7 -> {
+
+                }
+                case 8 -> {
+
+                }
+                case 9 -> {
+
+                }
+            }
+            //WAIT FOR REPLY THEN
+            //cardLayout.show(THE PANEL SELECTED HERE)
+            resultLabel.setText("Selected Option: " + selectedOption);
+            cardLayout.show(mainPanel, selPans[choice]);
+        });
+
+        selSide.add(sellerOptions);
+        selSide.add(resultLabel);
+        mainPanel.add(selSide);
+
+        //whatever option specific buttons and the like go into their
+        //own slot here:
+        //----------------------------------------------------------//
+        JPanel s0 = createCardPanel(selOp[0]);
+
+        //----------------------------------------------------------//
+        JPanel s1 = createCardPanel(selOp[1]);
+
+        //----------------------------------------------------------//
+        JPanel s2 = createCardPanel(selOp[2]);
+
+        //----------------------------------------------------------//
+        JPanel s3 = createCardPanel(selOp[3]);
+
+        //----------------------------------------------------------//
+        JPanel s4 = createCardPanel(selOp[4]);
+
+        //----------------------------------------------------------//
+        JPanel s5 = createCardPanel(selOp[5]);
+
+        //----------------------------------------------------------//
+        JPanel s6 = createCardPanel(selOp[6]);
+
+        //----------------------------------------------------------//
+        JPanel s7 = createCardPanel(selOp[7]);
+
+        //----------------------------------------------------------//
+        JPanel s8 = createCardPanel(selOp[8]);
+
+        //----------------------------------------------------------//
+        JPanel s9 = createCardPanel(selOp[9]);
+
+        //----------------------------------------------------------//
+
+        mainPanel.add(s0, "s0");
+        mainPanel.add(s1, "s1");
+        mainPanel.add(s2, "s2");
+        mainPanel.add(s3, "s3");
+        mainPanel.add(s4, "s4");
+        mainPanel.add(s5, "s5");
+        mainPanel.add(s6, "s6");
+        mainPanel.add(s7, "s7");
+        mainPanel.add(s8, "s8");
+        mainPanel.add(s9, "s9");
+
+        frame.add(mainPanel);
 
     }
     private static JPanel createCardPanel(String text) {
