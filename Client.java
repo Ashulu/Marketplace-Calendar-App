3205d24839e1d6ff7b6ca7ad603baa34b944b597
anoctopi <s-ialam@lwsd.org>
2023-11-29 01:26:04 -0500

    added format of switching between jframes in cardlayout

--- Client.java
+++ Client.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
+import java.util.*;
 public class Client extends JComponent implements Runnable {
     public Client() {
     }
@@ -12,12 +12,77 @@ public class Client extends JComponent implements Runnable {
 
     public void run() {
         JFrame frame = new JFrame("Calendar");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
         Container content = frame.getContentPane();
         content.setLayout(new BorderLayout());
 
         frame.setSize(600, 400);
         frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        JPanel mainPanel = new JPanel(new CardLayout());
+        CardLayout cardLayout = (CardLayout)  mainPanel.getLayout();
+
+        String[] selOp = new String[] {
+                "View Approved", "Appointment Requests",
+                "Create Store", "Create Calendar",
+                "Edit Calendar", "Delete Calendar",
+                "Show Statistics", "Import Calendar",
+                "Logout", "Quit"
+        };
+        String[] selPans = new String[] {
+                "s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9"
+        };
+
+        JPanel selSide = createCardPanel("Calendar: Seller");
+        JButton confirm = new JButton("Confirm");
+        //----------------------------------------------------------//
+        selSide.add(confirm);
+        //add this button to every single card - it's going to be
+        //what sends the request
+
+        //----------------------------------------------------------//
+        JComboBox<String> sellerOptions = new JComboBox<>(selOp);
+        JLabel resultLabel = new JLabel("");
+        confirm.addActionListener(e -> {
+            String selectedOption = (String) sellerOptions.getSelectedItem();
+            //SEND REQUEST HERE
+            //
+            //WAIT FOR REPLY THEN
+            //cardLayout.show(THE PANEL SELECTED HERE)
+            resultLabel.setText("Selected Option: " + selectedOption);
+            cardLayout.show(mainPanel, selPans[Arrays.asList(selOp).indexOf(selectedOption)]);
+        });
+
+        selSide.add(sellerOptions);
+        selSide.add(resultLabel);
+        mainPanel.add(selSide);
+        //----------------------------------------------------------//
+        JPanel s0 = createCardPanel(selOp[0]);
+
+        //----------------------------------------------------------//
+        JPanel s1 = createCardPanel(selOp[1]);
+
+        //----------------------------------------------------------//
+        JPanel s2 = createCardPanel(selOp[2]);
+
+        //----------------------------------------------------------//
+        JPanel s3 = createCardPanel(selOp[3]);
+
+        //----------------------------------------------------------//
+        //etc etc
+        //----------------------------------------------------------//
+
+
+        mainPanel.add(s0, "s0");
+        mainPanel.add(s1, "s1");
+        mainPanel.add(s2, "s2");
+        mainPanel.add(s3, "s3");
+        frame.add(mainPanel);
+
+        //maybe exit works?
+        frame.setDefaultCloseOperation(exit());
         frame.setVisible(true);
 
         //TODO: GUI for login and create account
@@ -62,5 +127,16 @@ public class Client extends JComponent implements Runnable {
                 frame.add(cb);
             }
         });
+
+    }
+    private static JPanel createCardPanel(String text) {
+        JPanel panel = new JPanel();
+        panel.add(new JLabel(text));
+        return panel;
+    }
+
+    public static int exit() {
+        //send exit message here
+        return JFrame.DISPOSE_ON_CLOSE;
     }
 }
