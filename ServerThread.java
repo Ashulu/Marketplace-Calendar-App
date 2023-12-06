a201229846f79fe1da7288fd6aa2cd14bd0ee0f1
Gunyoung <park1659@purdue.edu>
2023-12-05 22:48:33 -0500

    login

--- ServerThread.java
+++ ServerThread.java
@@ -38,10 +38,36 @@ public class ServerThread extends Thread {
             try {
                 statement = connection.createStatement();
                 clientAction = reader.readLine();
-                String[] inputList = clientAction.split(" ");
 
                 switch (clientAction) {
                     case "login":
+                        int returning = 0;
+                        String dbPassword = null;
+                        String dbType = null;
+
+                        String input = reader.readLine();
+                        String[] inputList = input.split(",");
+                        String query = String.format("SELECT password FROM accounts WHERE email == %s", inputList[0]);
+                        ResultSet result = statement.executeQuery(query);
+                        if (result.next()) {
+                            dbPassword = result.getString("password");
+                            if (dbPassword.equals(inputList[1])) {
+                                dbType = result.getString("type");
+                                if (dbType.equals("customer")) { // type customer
+                                    returning = 1;
+                                } else { // type seller
+                                    returning = 2;
+                                }
+                            } else { // password is incorrect
+                                returning = 0;
+                            }
+                        } else { //email does not exist
+                            returning = -1;
+                        }
+
+
+
+
                         break;
                     case "createAccount":
                         break;
@@ -82,7 +108,7 @@ public class ServerThread extends Thread {
                 System.out.println("Could not pass data to / receive data from client");
                 return;
             } catch (SQLException e) {
-                System.out.println("Error when executing statement. Statement was: " + clientInput);
+                System.out.println("Error when executing statement. Statement was: " + clientAction);
                 e.printStackTrace();
                 return;
             }
